a9190fda74efa3f9eb0a9f0963dc7ba64a60af3b
Gabriel Hirano <69726695+gabrielhirano@users.noreply.github.com>
2023-09-03 00:41:26 -0300

    change name api base

--- environment_settings/environment.dart
+++ environment_settings/environment.dart
@@ -4,21 +4,21 @@ class Environment {
   static final dev = AppConfig(
     appName: 'Meu App Dev',
     appEnvironment: AppEnvironment.development,
-    apiBaseUrl: 'dev.apparaujo.araujo.com.br',
+    apiBaseUrl: 'dev.api.base.com.br',
     timeout: 120000,
   );
 
   static final hml = AppConfig(
     appName: 'Meu App Hml',
     appEnvironment: AppEnvironment.homolog,
-    apiBaseUrl: 'hml.apparaujo.araujo.com.br',
+    apiBaseUrl: 'hml.api.base.com.br',
     timeout: 120000,
   );
 
   static final prod = AppConfig(
     appName: 'Meu App',
     appEnvironment: AppEnvironment.production,
-    apiBaseUrl: 'apparaujo.araujo.com.br',
+    apiBaseUrl: 'api.base.com.br',
     timeout: 120000,
   );
 }
